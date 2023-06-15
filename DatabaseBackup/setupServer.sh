78c67a86168a7c0974b48644649457cb0d1202c4
Augustsabr <augustsabr@gmail.com>
2023-06-15 14:05:30 +0200

    ah

--- DatabaseBackup/setupServer.sh
+++ DatabaseBackup/setupServer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apacheServerName=$1
 
 apt update
 apt install ufw
@@ -9,24 +10,24 @@ ufw allow OpenSSH
 ufw allow in "WWW Full"
 
 apt install apache2 -y
-mkdir -p /var/www/Arsoppgave
-chown -R l2 /var/www/Arsoppgave
-chmod -R 755 /var/www/Arsoppgave
-cat >> /etc/apache2/sites-available/Arsoppgave.conf << EOF
+mkdir -p /var/www/$apacheServerName
+chown -R l2 /var/www/$apacheServerName
+chmod -R 755 /var/www/$apacheServerName
+cat >> /etc/apache2/sites-available/$apacheServerName.conf << EOF
 <VirtualHost *:80>
     ServerAdmin webmaster@localhost
-    ServerName Arsoppgave
-    ServerAlias www.Arsoppgave
-    DocumentRoot /var/www/Arsoppgave
+    ServerName $apacheServerName
+    ServerAlias www.$apacheServerName
+    DocumentRoot /var/www/$apacheServerName
     ErrorLog ${APACHE_LOG_DIR}/error.log
     CustomLog ${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>
 EOF
-a2ensite Arsoppgave.conf
+a2ensite $apacheServerName.conf
 a2dissite 000-default.conf
 
 apt-get install git git-core -y
-git clone https://github.com/AugustSabr/JavaGame_AdminSite.git /var/www/Arsoppgave/
+git clone https://github.com/AugustSabr/JavaGame_AdminSite.git /var/www/$apacheServerName/
 
 apt install php -y
 
@@ -40,20 +41,20 @@ sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgres
 ufw allow 5432
 
 apt install -y phppgadmin apache2 -y
-psql -U postgres -d myDataBase -a -f myInsertFile
+
+psql postgres < /home/l2/DatabaseBackup/DBusers.sql postgres
+psql game4 < /home/l2/DatabaseBackup/dump.sql postgres
+
 sed -i 's/\(^.*conf\[.extra_login_security.\] =\) true/\1 false/' /etc/phppgadmin/config.inc.php
 sed -i 's/Require local/Require all granted/' /etc/apache2/conf-enabled/phppgadmin.conf
-
 
 sed -i 's/peer/md5/' /etc/postgresql/15/main/pg_hba.conf
 sed -i 's/scram-sha-256/md5/' /etc/postgresql/15/main/pg_hba.conf
 sed -i 's#127.0.0.1/32#0.0.0.0/0#' /etc/postgresql/15/main/pg_hba.conf
 sudo service postgresql restart
 
-psql postgres < /home/l2/DatabaseBackup/DBusers.sql postgres
-psql game4 < /home/l2/DatabaseBackup/dump.sql postgres
-
-systemctl restart apache2 -y
-/etc/init.d/postgresql restart -y
+systemctl restart apache2
+/etc/init.d/postgresql restart
 ufw enable
-systemctl restart networking.service -y
+systemctl restart networking.service
+sudo reboot
