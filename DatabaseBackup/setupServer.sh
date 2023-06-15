1e939916371bc1ac9f678d147aeed4585b95392d
Augustsabr <augustsabr@gmail.com>
2023-06-15 12:31:03 +0200

    ah

--- DatabaseBackup/setupServer.sh
+++ DatabaseBackup/setupServer.sh
@@ -36,16 +36,24 @@ sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add - 
 apt update
 apt-get -y install postgresql-15 -y
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/15/main/postgresql.conf
 ufw allow 5432
 
 apt install -y phppgadmin apache2 -y
+psql -U postgres -d myDataBase -a -f myInsertFile
 sed -i 's/\(^.*conf\[.extra_login_security.\] =\) true/\1 false/' /etc/phppgadmin/config.inc.php
 sed -i 's/Require local/Require all granted/' /etc/apache2/conf-enabled/phppgadmin.conf
 
-\i /home/l2/DatabaseBackup/DBusers.sql
-\i /home/l2/DatabaseBackup/dump.sql
 
-systemctl restart apache2
-/etc/init.d/postgresql restart
+sed -i 's/peer/md5/' /etc/postgresql/15/main/pg_hba.conf
+sed -i 's/scram-sha-256/md5/' /etc/postgresql/15/main/pg_hba.conf
+sed -i 's#127.0.0.1/32#0.0.0.0/0#' /etc/postgresql/15/main/pg_hba.conf
+sudo service postgresql restart
+
+psql postgres < /home/l2/DatabaseBackup/DBusers.sql postgres
+psql game4 < /home/l2/DatabaseBackup/dump.sql postgres
+
+systemctl restart apache2 -y
+/etc/init.d/postgresql restart -y
 ufw enable
-systemctl restart networking.service
+systemctl restart networking.service -y
